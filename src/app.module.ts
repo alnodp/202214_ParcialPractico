b0e75ecb3b29995d91cad955188a38b8342a696a
Andres Lopez <am.lopezr1@uniandes.edu.co>
2022-09-18 19:24:37 -0500

    creando modulos base e instalando typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocioModule } from './socio/socio.module';
+import { ClubModule } from './club/club.module';
+import { SocioClubModule } from './socio-club/socio-club.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'clubsocial',
+      entities: [],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+    SocioModule,
+    ClubModule,
+    SocioClubModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
